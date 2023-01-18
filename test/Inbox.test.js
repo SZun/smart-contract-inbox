f1c83d6a09704905fc553aa32886f71ab861bd58
Sam Zun <zunsamuel97@gmail.com>
2023-01-18 17:28:29 -0600

    added second test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,19 +4,27 @@ const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 const {interface, bytecode} = require('../compile')
 
-let accounts
+let accounts,inbox;
+const INITIAL_MESSAGE = 'Hi there!'
 
 beforeEach(async () => {
 
     accounts = await web3.eth.getAccounts()
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi there!']})
+        .deploy({data: bytecode, arguments: [INITIAL_MESSAGE]})
         .send({from: accounts[0], gas: '1000000'})
 })
 
 describe('Inbox', () => {
+    
     it('deploys a contract', () => {
-        console.log(inbox)
+        assert.ok(inbox.options.address)
     })
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message, INITIAL_MESSAGE)
+    })
+    
 })
