4c8542a64db2f61d43a8c333f7d87a54b72b50da
Sam Zun <zunsamuel97@gmail.com>
2023-01-18 17:53:32 -0600

    updated files to work with newer solidity version

--- deploy.js
+++ deploy.js
@@ -1,27 +1,26 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-const Web3 = require('Web3')
-const {interface, bytecode} = require('./compile')
-
-const provider = new HDWalletProvider(
-    'regret street tragic include amount deer delay vacant climb subject stuff photo',
-    'https://goerli.infura.io/v3/9b3c6e68269b4c2c8bce764c44c778cd'
-)
-
-const web3 = new Web3(provider)
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+  'YOUR_MNEMONIC',
+  'YOUR_INFURA_URL'
+);
+ 
+const web3 = new Web3(provider);
+ 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts()
-    const firstAccount = accounts[0]
-
-    console.log('Attempting to deploy from account', firstAccount)
-
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data: bytecode, arguments: ['Hi there!']})
-        .send({gas: '1000000', from: firstAccount})
-
-    console.log('Contract deployed to', result.options.address)
-
-    provider.engine.stop()
-}
-
-deploy()
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
